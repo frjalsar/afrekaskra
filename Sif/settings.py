a0fb9307dc5d23f1f2e39c7705d8727d0ce88aab
Kristinn Torfason <kristinnt@ru.is>
2020-05-01 00:39:29 +0000

    On branch master  Changes to be committed: 	modified:   Sif/settings.py 	modified:   templates/top_lists.html

--- Sif/settings.py
+++ Sif/settings.py
@@ -24,7 +24,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SIF_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if 'SIF_ON_HEROKU' in os.environ:
+    DEBUG = FALSE
+else:
+    DEBUG = True
 
 ALLOWED_HOSTS = [] # The django_heroku package takes care of this.
 
