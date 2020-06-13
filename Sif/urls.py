1a252653df60ccb5bd6edb228770e5a37a7b82f3
Kristinn Torfason <kristinnt@ru.is>
2020-06-13 16:49:58 +0000

    Upphaf breytinga á að færa front end alveg yfir í Vue.
     On branch master
     Changes to be committed:
    	modified:   Sif/urls.py
    	renamed:    static/fri-logo.png -> static/bak/fri-logo.png
    	renamed:    static/kt_action.jpg -> static/bak/kt_action.jpg
    	renamed:    static/kt_profile.jpg -> static/bak/kt_profile.jpg
    	renamed:    static/vue-spinner.js -> static/bak/vue-spinner.js
    	new file:   static/fa-brands-400.088c083c.woff2
    	new file:   static/fa-brands-400.16abc57d.woff
    	new file:   static/fa-brands-400.bd9767a7.ttf
    	new file:   static/fa-brands-400.c2094a80.eot
    	new file:   static/fa-brands-400.ddedbaaa.svg
    	new file:   static/fa-regular-400.653ccd4f.eot
    	new file:   static/fa-regular-400.78e93500.svg
    	new file:   static/fa-regular-400.8f5655b0.ttf
    	new file:   static/fa-regular-400.cf1943ea.woff2
    	new file:   static/fa-regular-400.eeedd9bc.woff
    	new file:   static/fa-solid-900.1afd2853.woff
    	new file:   static/fa-solid-900.55af4fc1.woff2
    	new file:   static/fa-solid-900.680d365a.svg
    	new file:   static/fa-solid-900.75bb2664.eot
    	new file:   static/fa-solid-900.9f781533.ttf
    	new file:   static/fri-logo.3c8a482b.png
    	new file:   static/front.26c70d90.js
    	new file:   static/index.html
    	new file:   static/main.aa102ac8.css
    	new file:   static/main.aa102ac8.css.map
    	new file:   static/main.c7afc8f0.js
    	new file:   static/main.c7afc8f0.js.map
    	new file:   static/toplists.8a07096b.css
    	new file:   static/toplists.c19cb411.js
    	new file:   static/toplists.c19cb411.js.map
    	new file:   templates/index.html

--- Sif/urls.py
+++ Sif/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, re_path
 from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 import os
 
 from Sif import views
@@ -26,7 +27,8 @@ from Sif import api_views
 urlpatterns = [
     # Views
     #path('admin/', admin.site.urls),
-    path(r'', views.front_page, name='front_page'),
+    #path(r'', views.front_page, name='front_page'),
+    path(r'', TemplateView.as_view(template_name='index.html')),
     re_path(r'^keppandi/*', views.competitor, name='competitor'),
     re_path(r'^top/*', views.top_lists, name="top_lists"),
 
