7586171c8d1d30308d6a3df37723f0afd873aa0e
Kristinn Torfason <kristinnt@ru.is>
2020-06-13 18:40:25 +0000

    Test.
     On branch master
     Changes to be committed:
    	modified:   src/top/toplists.vue
    	deleted:    static/main.05573ea7.css
    	deleted:    static/main.05573ea7.css.map
    	deleted:    static/main.9ceef5d2.js
    	deleted:    static/main.9ceef5d2.js.map
    	modified:   static/main.b6535970.js
    	deleted:    static/toplists.1a3c920c.js.map
    	new file:   static/toplists.6ac3425d.css
    	deleted:    static/toplists.db8467cd.css
    	renamed:    static/toplists.1a3c920c.js -> static/toplists.f003d023.js
    	new file:   static/toplists.f003d023.js.map

--- src/top/toplists.vue
+++ src/top/toplists.vue
@@ -353,6 +353,22 @@
 import axios from "axios"
 import PulseLoader from "vue-spinner/src/PulseLoader.vue"
 
+      $('.dropdown-menu a.dropdown-toggle').on('click', function (e) {
+        if (!$(this).next().hasClass('show')) {
+          $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+        }
+        var $subMenu = $(this).next(".dropdown-menu");
+        $subMenu.toggleClass('show');
+    
+    
+        $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function (e) {
+          $('.dropdown-submenu .show').removeClass("show");
+        });
+    
+    
+        return false;
+      });
+
 export default {
   name: "TopLists",
   components: {
@@ -834,7 +850,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .dropdown-menu-long {
   max-height: 500px;
   overflow-y: auto;
