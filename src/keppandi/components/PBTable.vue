1f79e9bdbc1fd0fe6a6ccc2eb8b3b12437edaf8b
Kristinn Torfason <kristinnt@ru.is>
2020-07-23 14:41:33 +0000

    PB table now a component.
     On branch master
     Changes to be committed:
    	new file:   keppandi/components/PBTable.vue
    	modified:   keppandi/single.vue

---
+++ src/keppandi/components/PBTable.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <table class="table table-striped table-hover table-responsive-sm table-sm">
+      <col span="1" class="wide" />
+      <thead>
+        <tr>
+          <th scope="col">Grein</th>
+          <th scope="col">PB úti</th>
+          <th scope="col">PB inni</th>
+          <th scope="col">Fjöldi</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="(i, index) in event_info"
+          v-show="(index < 5) || showAllEvents"
+          :key="i.Event"
+          @click.prevent="onClick && onClick(i)"
+        >
+          <!-- v-bind:style="{display: 'none'}" -->
+          <th scope="row">{{i.EventShortName}} [{{i.EventUnit}}]</th>
+          <td>{{i.PB_out}}</td>
+          <td>{{i.PB_in}}</td>
+          <td>{{i.count}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["data", "showAllEvents", "competitorID"],
+  computed: {
+      event_info() {
+          return this.data
+      }
+  },
+  methods: {
+    onClick(item) {
+      //alert(item.EventID);
+      //this.$router.push("/keppandi/" + this.competitorID + "/" + item.EventID)
+      this.$router.push({
+        name: "CompetitorEvent",
+        params: { competitorID: this.competitorID, eventID: item.EventID },
+      });
+    },
+  },
+};
+</script>
--- src/keppandi/single.vue
+++ src/keppandi/single.vue
@@ -26,31 +26,7 @@
           </div>
         </div>
         <div class="card-body">
-          <table class="table table-striped table-hover table-responsive-sm table-sm">
-            <col span="1" class="wide" />
-            <thead>
-              <tr>
-                <th scope="col">Grein</th>
-                <th scope="col">PB úti</th>
-                <th scope="col">PB inni</th>
-                <th scope="col">Fjöldi</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr
-                v-for="(i, index) in event_info"
-                v-show="(index < 5) || showAllEvents"
-                :key="i.Event"
-                @click.prevent="onClick && onClick(i)"
-              >
-                <!-- v-bind:style="{display: 'none'}" -->
-                <th scope="row">{{i.EventShortName}} [{{i.EventUnit}}]</th>
-                <td>{{i.PB_out}}</td>
-                <td>{{i.PB_in}}</td>
-                <td>{{i.count}}</td>
-              </tr>
-            </tbody>
-          </table>
+          <pbtable :data="event_info" :showAllEvents="showAllEvents" :competitorID="competitorID" ref="pbtable"></pbtable>
           <piechart :data="pieData" ref="pieChart" v-show="!showAllEvents"></piechart>
         </div>
         <div class="card-footer text-muted text-center">
@@ -66,12 +42,14 @@ import axios from "axios";
 //import { Chart } from "highcharts-vue";
 import PulseLoader from "vue-spinner/src/PulseLoader.vue";
 import PieChart from "./components/PieChart.vue";
+import PBTable from "./components/PBTable.vue";
 
 export default {
   name: "KeppandiSingle",
   components: {
     //highcharts: Chart,
     piechart: PieChart,
+    pbtable: PBTable,
     PulseLoader,
   },
   data() {
@@ -131,14 +109,6 @@ export default {
     }
   },
   methods: {
-    onClick(item) {
-      //alert(item.EventID);
-      //this.$router.push("/keppandi/" + this.competitorID + "/" + item.EventID)
-      this.$router.push({
-        name: "CompetitorEvent",
-        params: { competitorID: this.competitorID, eventID: item.EventID },
-      });
-    },
     get_data: function () {
       this.$parent.loading = true;
       this.message = "Næ í gögn ekki stökkva langt 😉";
@@ -162,7 +132,7 @@ export default {
               " (" +
               this.competitor_info.Club +
               ")";
-            //this.processData();
+
           })
         )
         .catch((error) => {
@@ -176,38 +146,6 @@ export default {
           this.isReady = true;
         });
     },
-/*     processData: function () {
-      //console.log("Process data");
-      var dataLen = this.event_info.length;
-
-      let total = 0;
-      for (var i = 0; i < dataLen; i++) {
-        total = total + this.event_info[i].count;
-      }
-
-      let data_points = [];
-      let other = 0;
-      let per = 0;
-      for (var i = 0; i < dataLen; i++) {
-        per = (this.event_info[i].count / total) * 100;
-
-        if (per < 1.5) {
-          other = other + per;
-        } else {
-          data_points.push({
-            name: this.event_info[i].EventShortName,
-            y: per,
-          });
-        }
-      }
-
-      if (other > 0) {
-        data_points.push({ name: "Aðrar greinar", y: other });
-      }
-
-      //console.log(data_points);
-      this.chartOptions.series[0].data = data_points;
-    }, */
     toggle_showEvents: function (event) {
       this.showAllEvents = !this.showAllEvents;
     },
