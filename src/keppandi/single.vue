ef5d6eded64389c0ac0c6cbe3a1fe7c1348ee49c
Kristinn Torfason <kristinnt@ru.is>
2020-09-21 20:46:13 +0000

    Font size changes on profile page

--- src/keppandi/single.vue
+++ src/keppandi/single.vue
@@ -10,10 +10,10 @@
       <div class="action-div">
         <img class="img-fluid img-action" v-bind:src="'/api/img/action/' + competitorID" />
         <div class="topleft">
-          <h2>
+          <h2 style="font-size:3.5vw;">
             <b>{{competitor_info.FirstName}} {{competitor_info.LastName}}</b>
           </h2>
-          <h5>
+          <h5 style="font-size:2.5vw;">
             {{competitor_info.Club}} -
             <i
               class="fas"
