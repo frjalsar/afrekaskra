e5abce0b032848e32229cbc28bb68b5798631256
Kristinn Torfason <kristinnt@ru.is>
2022-12-29 17:48:40 +0000

    Removed Heroku package in Django settings.py

--- Sif/settings.py
+++ Sif/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
+#import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -221,4 +221,4 @@ STATICFILES_DIRS = (
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Activate Django-Heroku. We have our own database settings and we set the private key ourselves.
-django_heroku.settings(locals(), databases=False, secret_key=False, logging=False)
+#django_heroku.settings(locals(), databases=False, secret_key=False, logging=False)
