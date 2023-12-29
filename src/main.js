1268bd1a1d28fa77b62d0ec4144cba87ea46aab2
Kristinn Torfason <kristinnt@ru.is>
2023-12-29 20:00:40 +0000

    vue-gtag added

--- src/main.js
+++ src/main.js
@@ -8,8 +8,7 @@ import HighchartsVue from "highcharts-vue";
 import StockModule from "highcharts/modules/stock";
 import Exporting from 'highcharts/modules/exporting';
 import OfflineExporting from "highcharts/modules/offline-exporting";
-//import VueGtag from "vue-gtag";
-//import VueMeta from "vue-meta";
+import VueGtag from "vue-gtag";
 
 //Vue.config.productionTip = true
 
@@ -27,15 +26,21 @@ moment.locale('is');
   }
 })*/
 
-//Vue.use(VueGtag, {
-//  config: { id: "G-ZHPYDECZRM" }
-//}, router);
-
 const app = createApp(App)
 //Vue.configureCompat({ COMPONENT_ASYNC: false, // Datepicker compatibility
 //                      RENDER_FUNCTION: false // Remove some warnings
 //                    }) 
-//app.config.globalProperties.global_api_url_prefix = 'https://sif.fri.is' // For production
-//app.config.globalProperties.global_api_url_prefix = 'http://127.0.0.1:8000' // For development
-app.config.globalProperties.global_api_url_prefix = '' // For production
+
+// Check if running in development environment
+if (process.env.NODE_ENV === 'development') {
+  app.config.globalProperties.global_api_url_prefix = 'http://127.0.0.1:8000' // For development
+} else {
+  app.config.globalProperties.global_api_url_prefix = '' // For production
+  //app.config.globalProperties.global_api_url_prefix = 'https://sif.fri.is' // For production
+  // Set g-tag id
+  app.use(VueGtag, {
+    config: { id: "G-ZHPYDECZRM" }
+  });
+}
+
 app.use(HighchartsVue).use(router).mount('#app')
